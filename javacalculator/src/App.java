8a618317a46e4fa446c460f05b09cb7093072101
kassigane <113329636+kassigane@users.noreply.github.com>
2023-08-21 18:14:56 +0300

    Update App.java

--- javacalculator/src/App.java
+++ javacalculator/src/App.java
@@ -1,46 +1,45 @@
 import java.util.Scanner;
 
+/**
+ * This class implements a simple calculator that takes user input for an operator
+ * and two numbers, performs the chosen arithmetic operation, and displays the result.
+ */
 class Main {
   public static void main(String[] args) {
-
+    
+    //Declare variables to store the operators
     char operator;
     Double number1, number2, result;
 
-    // create an object of Scanner class
+    // Create an object of Scanner class to read the user inputs
     Scanner input = new Scanner(System.in);
 
-    // ask users to enter operator
+    // Prompt the user to enter an operator
     System.out.println("Choose an operator: +, -, *, or /");
     operator = input.next().charAt(0);
 
-    // ask users to enter numbers
+    // Promt the user to enter the first number
     System.out.println("Enter first number");
     number1 = input.nextDouble();
 
+    //Prompt the user to enter the second number
     System.out.println("Enter second number");
     number2 = input.nextDouble();
 
-    switch (operator) {
-
-      // performs addition between numbers
+    //Perform arithmetic operations based on the chosen operator
+    switch (operator) {  
       case '+':
         result = number1 + number2;
         System.out.println(number1 + " + " + number2 + " = " + result);
         break;
-
-      // performs subtraction between numbers
       case '-':
         result = number1 - number2;
         System.out.println(number1 + " - " + number2 + " = " + result);
         break;
-
-      // performs multiplication between numbers
       case '*':
         result = number1 * number2;
         System.out.println(number1 + " * " + number2 + " = " + result);
         break;
-
-      // performs division between numbers
       case '/':
         if (number2 != 0) {
         result = number1 / number2;
@@ -49,12 +48,12 @@ class Main {
         System.out.println("Cannot divide by zero");
         }
         break;
-
       default:
         System.out.println("Invalid operator!");
         break;
     }
 
+    //Close the Scanner object to release resources
     input.close();
   }
 }
